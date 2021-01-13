1166345b15af5167c8b8629f5db386a0cdf4b5b3
AntonioFuziy <antoniovf@al.insper.edu.br>
2021-01-13 16:24:26 -0300

    first code and README

---
+++ README.md
@@ -0,0 +1,36 @@
+## VR Robot - SMASH Robotics Insper
+
+**Packages:**
+
+  `pip install pillow`
+  `pip install matplotlib`
+
+**Terminal commands to access your IP_ADDRESS:**
+
+  **Windows:**
+  
+  `ipconfig`
+
+  **Linux:**
+  
+  `hostname -I`
+
+**Paste your IP_ADDRESS on the IP.py file string**
+  
+  `IP_ADDRESS = "YOUR_IP_ADDRESS"`
+
+**Run this command on your computer (SERVER SIDE):**
+
+  `python server.py`
+  
+  **or**
+  
+  `python3 server.py`
+
+**Finally run this command on your Raspberry Pi (CLIENT SIDE):**
+
+  `python client.py`
+  
+  **or**
+  
+  `python3 client.py`
---
+++ server.py
@@ -0,0 +1,39 @@
+import io
+import socket
+import struct
+from PIL import Image
+import matplotlib.pyplot as plt
+from IP import IP_ADDRESS
+
+server_socket = socket.socket()
+server_socket.bind(IP_ADDRESS, 8000)
+server_socket.listen(0)
+
+connection = server_socket.accept()[0].makefile("rb")
+try:
+  img = None
+  while True:
+    image_len = struct.unpack("<L", connection.read(struct.calcsize("<L")))[0]
+    if not image_len:
+      break
+
+    image_stream = io.BytesIO()
+    image_stream.write(connection.read(image_len))
+
+    image_stream.seek(0)
+    image = Image.open(image_stream)
+
+    if img is None:
+      img = plt.imshow(image)
+    else:
+      img.set_data(image)
+    
+    plt.pause(0.01)
+    plt.draw()
+    
+    print(f"Image is {image.size}")
+    image.verify()
+    print("Image is verified")
+finally:
+  connection.close()
+  server_socket.close()
